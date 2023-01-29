18b94b360040e43cc76a2968c2b81619e613d3a4
Alex Ntavelos <a.ntavelos@gmail.com>
2023-01-29 11:15:34 +0100

    open archived version in new tab

--- redirect.js
+++ redirect.js
@@ -1,1 +1,2 @@
-window.location.href = `${window.location.protocol}//archive.is/${window.location.host}${window.location.pathname}`;
+// window.location.href = `${window.location.protocol}//archive.is/${window.location.host}${window.location.pathname}`;
+window.open(`${window.location.protocol}//archive.is/${window.location.host}${window.location.pathname}`, '_blank');
